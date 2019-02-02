16c4e7b71ad04e217e8ff32c11e7ce04a97aae3e
Nashid Muhammed <Nashid Muhammed@NASZI>
2019-02-02 17:20:58 +0530

    up

--- websvc/Controllers/TurfBookingController.cs
+++ websvc/Controllers/TurfBookingController.cs
@@ -13,9 +13,15 @@ namespace websvc.Controllers
            TurfBooking[] turfBooking = new TurfBooking[]
             {
                 new TurfBooking { Id =1,TurfMaster =new TurfMaster{Id=1,Description="Bridge", Location="Feroke"} },
-                new TurfBooking {Id =2,TurfMaster=new TurfMaster{Id=2, Name="FootOut",  Location="Petta"} },
+                new TurfBooking {Id =2,TurfMaster=new TurfMaster{Id=2, Name="FootOut",  Location="Petta", } },
                 new TurfBooking {Id =3,TurfMaster=new TurfMaster{Id=3, Name="Game5",  Location="Chungham"} },
-                new TurfBooking{Id =4,TurfMaster=new TurfMaster{Id=4, Name="fifa",  Location="ramanattukara"} }
+                new TurfBooking{Id =4,TurfMaster=new TurfMaster{Id=4, Name="fifa",  Location="ramanattukara", TypeofTurf=new TypeofTurf{ Id=1, Description="5s"} } },
+                new TurfMaster{ Id=5, Location="kozhikkode", Name="grand soccer", Description="5s,7s", TypeofTurf=new TypeofTurf{ Id=2 } } ,
+               
+            
+
+
+            
             };
 
             public TurfBooking[] Get()
